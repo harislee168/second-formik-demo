037e291bdfeb8652455721e4f8e3b18663f728f3
Haris Lee <haris.lee168@gmail.com>
2023-07-30 11:30:46 +0800

    formik container

---
+++ src/components/FormikContainer.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {Formik, Form} from 'formik'
+import * as Yup from 'yup'
+
+function FormikContainer() {
+  const initialValues = {}
+  const validationSchema = Yup.object({})
+  const onSubmit = (values) => {
+    console.log(values)
+  }
+  return (
+    <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+      {
+        (formik) => {
+          return (
+            <Form>
+              <button type='submit'>Submit</button>
+            </Form>
+          )
+        }
+      }
+    </Formik>
+  )
+}
+
+export default FormikContainer
